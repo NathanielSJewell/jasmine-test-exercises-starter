985dc088acc7187bc86a7f8e05fb88792d86c06b
Nathaniel Jewell <nathanielofboston@gmail.com>
2024-06-03 13:35:32 -0400

    test helpers.js functions

---
+++ tip-pool/helpers.test.js
@@ -0,0 +1,7 @@
+//test all functions in helpers.js file
+
+describe(`test all functions in helpers.js file`, function() {
+	const paymentObject = { tipAmt: 20, billAmt: 75, tipPercent: 15 };
+	sumPaymentTotal(paymentObject);
+	expect(total).toEqual();
+});
--- tip-pool/servers.test.js
+++ tip-pool/servers.test.js
@@ -1,17 +1,48 @@
-describe("Servers test (with setup and tear-down)", function() {
-  beforeEach(function () {
-    // initialization logic
-    serverNameInput.value = 'Alice';
-  });
+describe('Servers test (with setup and tear-down)', function() {
+	beforeEach(function() {
+		// initialization logic
+		serverNameInput.value = 'Alice';
+	});
 
-  it('should add a new server to allServers on submitServerInfo()', function () {
-    submitServerInfo();
+	it('should add a new server to allServers on submitServerInfo()', function() {
+		submitServerInfo();
 
-    expect(Object.keys(allServers).length).toEqual(1);
-    expect(allServers['server' + serverId].serverName).toEqual('Alice');
-  });
+		expect(Object.keys(allServers).length).toEqual(1);
+		expect(allServers['server' + serverId].serverName).toEqual('Alice');
+		//server1 : alice
+	});
 
-  afterEach(function() {
-    // teardown logic
-  });
+	afterEach(function() {
+		// teardown logic
+		serverNameInput.value = ``;
+		updateServerTable();
+		newTr.remove();
+	});
+});
+
+describe(`test servers.js functions`, function() {
+	beforeAll(function() {
+		allServers = {};
+		serverNameInput.value = 'Jacob';
+	});
+	it(`should add a new server of Jacob to allServers on submitServerInfo()`, function() {
+		submitServerInfo();
+
+		expect(Object.keys(allServers).length).toEqual(1);
+		expect(allServers['server' + serverId].serverName).toEqual('Jacob');
+		//allServers = serverName : `Jacob`;
+	});
+
+	it(`should test updateServerTable()`, function() {
+		submitServerInfo();
+		updateServerTable();
+		expect(serverTbody.contains(newTr)).toBeTruthy();
+	});
+
+	afterAll(function() {
+		// teardown logic
+		serverNameInput.value = ``;
+		updateServerTable();
+		newTr.remove();
+	});
 });
